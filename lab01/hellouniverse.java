e5d2fab44ebef63a04c1dd4c4b1fe8108ef42a5a
daniel park <daniel.park@ontariotechu.net>
2022-02-01 13:22:47 -0500

    new hellow message

--- lab01/hellouniverse.java
+++ lab01/hellouniverse.java
@@ -1,5 +1,5 @@
 public class HelloUniverse {
     public static void main(String[] args) {
-        System.out.println("Hello, universe! I'm a clone!"); 
-    }
+        System.out.println("Hello, universe! I'm not a clone anymore!"); 
+    
 }
