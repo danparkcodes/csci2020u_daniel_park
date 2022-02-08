effc9cfdb7530b567c69679520fec1ca32b96144
daniel park <daniel.park@ontariotechu.net>
2022-02-08 15:19:51 -0500

    Create and compile CSVDemo class

---
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,11 @@
+package lab02;
+
+public class SimpleTest {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SimpleTest().getGreeting());
+    }
+}
