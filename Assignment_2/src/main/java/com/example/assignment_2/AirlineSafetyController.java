5de3d79cd7eff7a153aecf975443f46386c61900
Daniel Park <daniel.park@ontariotechu.net>
2022-04-04 16:35:35 -0400

    Assignment 2 Airline Safety initialization

---
+++ Assignment_2/src/main/java/com/example/assignment_2/AirlineSafetyController.java
@@ -0,0 +1,14 @@
+package com.example.assignment_2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AirlineSafetyController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
