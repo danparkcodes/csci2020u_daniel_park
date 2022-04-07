bfd6644a359106a4df6ab11340c3e6d8a9a060d4
Daniel Park <daniel.park@ontariotechu.net>
2022-04-07 17:20:57 -0400

    inialize lab08 with JavaFX template

---
+++ lab09/src/main/java/com/example/lab09/HelloApplication.java
@@ -0,0 +1,45 @@
+package com.example.lab09;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class HelloApplication extends Application {
+    private string FILE_NAME;
+    private
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // This function takes a stock ticker symbol (e.g. GOOG), and downloads historical
+    // stock data about that organization from Yahoo Finance
+    public void downloadStockPrices() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            // handle exception
+        }
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
